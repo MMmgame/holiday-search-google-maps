cf1d796e3f3fd8d43621ae030f01a1d111feab26
Elinor Dowling <elinordowling@gmail.com>
2019-02-25 13:59:40 +0000

    Updated page style and map location

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,8 +1,8 @@
 function init() {
      var mapDiv = document.getElementById("mymap");
      var mapOptions = {
-       center: new google.maps.LatLng(37.09024, -119.4179324),  
-       zoom: 5,
+       center: new google.maps.LatLng(53.3453116,-6.2735728),  
+       zoom: 13,
        mapTypeId: google.maps.MapTypeId.ROADMAP
      };
      var map = new google.maps.Map(mapDiv, mapOptions);
