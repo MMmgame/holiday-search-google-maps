81dc65e4973d1169970ec5bb29a2bb42e29e6e0e
Elinor Dowling <elinordowling@gmail.com>
2019-03-04 15:04:12 +0000

    Added autocomplete search box to use the Google Places API

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,9 +1,30 @@
-function init() {
-     var mapDiv = document.getElementById("mymap");
+var map;
+function initMap() {
+     var mapDiv = document.getElementById('mymap');
      var mapOptions = {
        center: new google.maps.LatLng(53.3453116,-6.2735728),  
        zoom: 13,
        mapTypeId: google.maps.MapTypeId.ROADMAP
      };
-     var map = new google.maps.Map(mapDiv, mapOptions);
+     
+     map = new google.maps.Map(mapDiv, mapOptions);
+     
+
+     var defaultBounds = new google.maps.LatLngBounds(
+          new google.maps.LatLng(53.3461447,-6.2728271),
+          new google.maps.LatLng(53.3438193,-6.281254));
+          
+     var options = {
+          bounds: defaultBounds
+     };
+     
+     
+     
 }
+
+function activatePlacesSearch() {
+            var input = document.getElementById('searchBox');
+            var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+    activatePlacesSearch();
