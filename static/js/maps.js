5efe352d62b963e3ec2c7e2258e006c25414828e
Elinor Dowling <elinordowling@gmail.com>
2019-03-11 15:59:23 +0000

    Fixed autocomplete to bind map after a google places search from input box

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,30 +1,60 @@
-var map;
 function initMap() {
-     var mapDiv = document.getElementById('mymap');
-     var mapOptions = {
-       center: new google.maps.LatLng(53.3453116,-6.2735728),  
-       zoom: 13,
-       mapTypeId: google.maps.MapTypeId.ROADMAP
-     };
-     
-     map = new google.maps.Map(mapDiv, mapOptions);
-     
+        var map = new google.maps.Map(document.getElementById('mymap'), {
+          center: {lat: 53.3453116, lng: -6.2735728}, 
+          zoom: 13
+        });
+        
+        var input = document.getElementById('searchBox');
+        var types = document.getElementById('searchType');
+        var autocomplete = new google.maps.places.Autocomplete(input);
 
-     var defaultBounds = new google.maps.LatLngBounds(
-          new google.maps.LatLng(53.3461447,-6.2728271),
-          new google.maps.LatLng(53.3438193,-6.281254));
-          
-     var options = {
-          bounds: defaultBounds
-     };
-     
-     
-     
-}
+        autocomplete.bindTo('bounds', map);
 
-function activatePlacesSearch() {
-            var input = document.getElementById('searchBox');
-            var autocomplete = new google.maps.places.Autocomplete(input);
-}
+        // Set the data fields to return when the user selects a place.
+        autocomplete.setFields(
+            ['address_components', 'geometry', 'icon', 'name']);
 
-    activatePlacesSearch();
+        var infowindow = new google.maps.InfoWindow();
+        var infowindowContent = document.getElementById('infowindow-content');
+        infowindow.setContent(infowindowContent);
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
+
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+          }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(16);  
+          }
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+
+          infowindowContent.children['place-icon'].src = place.icon;
+          infowindowContent.children['place-name'].textContent = place.name;
+          infowindowContent.children['place-address'].textContent = address;
+          infowindow.open(map, marker);
+        });
+
+        
+      }
